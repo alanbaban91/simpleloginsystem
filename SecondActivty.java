65f13583373335be2cef5ba934e1abee9880b9f4
Alan Fuad Ali Baban <51624621+alanbaban91@users.noreply.github.com>
2019-06-10 13:26:06 +0300

    Create SecondActivty.java

---
+++ SecondActivty.java
@@ -0,0 +1,13 @@
+package alanfuad.com.loginsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec);
+    }
+}
